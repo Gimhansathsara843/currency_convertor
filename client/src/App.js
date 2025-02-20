fa3fd87dbaad9ad17feef6c29ba7726bcfa49e38
Gimhansathsara843 <160720393+Gimhansathsara843@users.noreply.github.com>
2025-02-20 15:13:31 +0530

    add the main header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import MainPage from './pages/mainPage';
+
 
 function App() {
   return (
-<div>hello</div>
+    <MainPage/>
   );
 }
 
---
+++ client/src/pages/mainPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function mainPage() {
+  return (
+    <div>
+        <h1>
+            Convert your currency today
+        </h1>
+        <p>
+            Welcome to "convert your currency today" This application allows you to easily convert currencies based on the latest exchange rate.
+            Whether you are planning to trip , managing your finances , or simply curious about the value of your oney in different currencies ,
+            this tool is here to help.
+        </p>
+    </div>
+  )
+}
